aabb158af8b3d2c38a8e86fd42bdb5c9089e6cb7
abebraham <j.abraham.gomez@gmail.com>
2018-06-09 17:21:27 -0700

    added helpful meta tags and title with description

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,17 @@
 class StaticPagesController < ApplicationController
 
 	def home
-
+		@title = "Home Page"
+		@desc = "description of website"
 	end
 
 	def help
-
+		@title = "Help Page"
+		@desc = "description of website"
 	end
 
 	def about
-
+		@title = "About Page"
+		@desc = "description of website"
 	end
 end
